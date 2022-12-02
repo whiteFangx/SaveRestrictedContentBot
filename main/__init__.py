1393e2fbac2898056a88aed9827e95b29fb8ea77
Riyan [wade] <86948336+whiteFangx@users.noreply.github.com>
2022-12-02 20:18:45 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
+API_ID = 4783634
+API_HASH = "f6c33f46599246676f75e153b615dbbc"
+BOT_TOKEN = "5282656364:AAGlZaYBX8yvzBmngA8L9CLWQej1HxfsMsc"
+SESSION = "BQAJr1LUg7YhUQe4n5oTkgttJjxMCCwODN8e_xTxpV7K9cGQtbxZa4jriVO0Zwve5ushrwa7CaFbSPYPP7Vvm9u-J1GxFwSv4Tj647DlqOygHLZJApJ-St3lvVu392o8sPyJ3TYVpPJQKZyo7dvdmU5JLrAwEkwdsy__3EkrVHvl5-fU6H0gWapnUpZ4PxmAM5dhQFOrGIcfwaGpTPvfBAVTSuZgacEauxI9sUNimS3aTg-WaJpq4agcVwPteU3hjO2Fugfk8SSuFxYQlcOYXAkeYVOYIKh1_yxyrLDWxYvngilhKYKxjatMLvoipMU36pUcXSX3_CW_mqoVONZu0VtCa3-QZQA"
 FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+AUTH = 1476517140
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
